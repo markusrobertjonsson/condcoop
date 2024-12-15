4fe8102812f5c3b243861b2a70c1e905b5139da4
Markus Jonsson <30292486+markusrobertjonsson@users.noreply.github.com>
2024-12-15 13:36:28 +0100

    Add docstrings

--- fig6.py
+++ fig6.py
@@ -15,6 +15,7 @@ FREE_RIDER = "FR"
 
 
 def sliding_average(values, sample_size):
+    '''Return the sliding average of the values in the specified list using depth sample_size.'''
     out = [None] * len(values)
     for i, val in enumerate(values):
         if i < sample_size:
@@ -25,11 +26,15 @@ def sliding_average(values, sample_size):
 
 
 class Distribution():
+    '''
+    A class representing a distribution of player types.
+    '''
+
     def __init__(self, uc, fr):
         assert(uc <= 1 and uc >= 0)
         assert(fr <= 1 and fr >= 0)
         assert(uc + fr <= 1)
-        self.uc = uc        
+        self.uc = uc
         self.fr = fr
         self.cc = 1 - (uc + fr)
 
@@ -57,6 +62,10 @@ class Distribution():
 
 
 class Population():
+    '''
+    A class representing a population of players.
+    '''
+
     def __init__(self, size, distribution):
         assert(size % 4 == 0)
         self.size = size
@@ -72,6 +81,10 @@ class Population():
 
 
 class Group():
+    '''
+    A class representing a group of players interacting in the public goods game.
+    '''
+
     def __init__(self, players):
         self.players = players  # List of Player objects
 
@@ -106,6 +119,10 @@ class Group():
 
 
 class Player():
+    '''
+    A class representing a player.
+    '''
+
     def __init__(self):
         self.last_contribution = None
 
@@ -127,6 +144,10 @@ class Player():
 
 
 class UnconditionalCooperator(Player):
+    '''
+    A class representing an unconditional player, inheriting from the Player class.
+    '''
+
     YINTERCEPT = {TREATMENT_10P: 17.51459397, TREATMENT_40P: 16.78084913,
                   TREATMENT_LEVEL: 18.70653308, TREATMENT_IMPACT: 17.3972948} #{CONTROL: 16.21719439, 
     SLOPE = {TREATMENT_10P: -0.065673995, TREATMENT_40P: 0.006177229,
@@ -150,6 +171,10 @@ class UnconditionalCooperator(Player):
 
 
 class ConditionalCooperator(Player):
+    '''
+    A class representing an conditional player, inheriting from the Player class.
+    '''
+
     YINTERCEPT = {TREATMENT_10P: 2.668407923, TREATMENT_40P: 1.984160769,
                   TREATMENT_LEVEL: -2.658563959, TREATMENT_IMPACT: 1.269983166}  # CONTROL: 2.060553218, 
     SLOPE = {TREATMENT_10P: 0.769928283, TREATMENT_40P: 0.80767842,
@@ -173,6 +198,10 @@ class ConditionalCooperator(Player):
 
 
 class FreeRider(Player):
+    '''
+    A class representing an free-riding player, inheriting from the Player class.
+    '''
+
     YINTERCEPT = {TREATMENT_10P: 4.562105652, TREATMENT_40P: 0.623097156,
                   TREATMENT_LEVEL: 4.182532558, TREATMENT_IMPACT: 7.031233664}  # CONTROL: 1.408893192
     SLOPE = {TREATMENT_10P: 0.161445329, TREATMENT_40P: 0.333341747,
@@ -196,6 +225,9 @@ class FreeRider(Player):
 
 
 class Simulation():
+    '''
+    A class representing a run of the agent-based simulation.
+    '''
     def __init__(self, size, distribution):
         self.population = Population(size, distribution)
 
@@ -213,6 +245,12 @@ class Simulation():
 
 
 def vary_only_uc():
+    '''
+    Vary the proportion of unconditional cooperators (UC) from 0 to 1 while keeping CC/FR
+    constant, run the simulation with this distribition and plot the proportion of successful
+    groups in the population as a function of UC.
+    '''
+
     def _compute_fr(uc):
         # Empirically: UC:0.56, CC:0.358, FR:0.035, so CC/FR=10.2
         fr = (1 - uc) / 11.2
@@ -239,53 +277,5 @@ def vary_only_uc():
     plt.show()
 
 
-def vary_uc_fr():
-    RESOLUTION = 100
-    ucs = [i / RESOLUTION for i in range(RESOLUTION + 1)]
-    frs = [i / RESOLUTION for i in range(RESOLUTION + 1)]
-
-    ps = []
-    for uc in ucs:
-        for fr in frs:
-            if uc + fr > 1:
-                p = 0
-            else:
-                distribution = Distribution(uc, fr)
-                simulation = Simulation(4000, distribution)
-                simulation.run(100)
-                p = simulation.get_proportion_successful_groups()
-            ps.append(p)
-            # print(f"{uc},{fr} => {p}")
-
-    ps = np.array(ps)
-    # maxp = max(ps)
-    Z = ps.reshape(len(ucs), len(frs))
-    # UC, FR = np.meshgrid(ucs, frs)
-    FR, UC = np.meshgrid(ucs, frs)
-
-    # print(UC)
-    # print(FR)
-    # print(Z)
-
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-
-    levels = np.linspace(0, 1, 11)
-    c = ax.contourf(UC, FR, Z, levels=levels, cmap=plt.get_cmap(name='jet', lut=1024))
-
-    # tickdiff = (levels[1] - levels[0]) / 2
-    cbarticks = [x for x in levels]
-    cbar = plt.colorbar(c, ticks=cbarticks, ticklocation=None)
-    cbarticklabels = [str(x) for x in levels]
-    cbar.set_ticklabels(cbarticklabels)  # vertically oriented colorbar
-    cbar.ax.tick_params(bottom=False, top=False, left=False, right=False, which='both')
-    
-    plt.title("Proportion successful groups")
-    plt.xlabel("$UC$")
-    plt.ylabel("$FR$")
-    plt.show()
-
-
 if __name__ == "__main__":
-    # vary_uc_fr()
     vary_only_uc()
